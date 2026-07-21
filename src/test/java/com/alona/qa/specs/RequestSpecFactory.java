a23d4c8aace4f3999dcb0705c89042e06bc9fe26
Alona Zakharchenko <alona.zakharchenko@ifd-gmbh.com>
2026-07-21 12:01:24 +0200

    fix: restore ObjectMapperConfig.GSON to ensure Gson is used for deserialization

--- src/test/java/com/alona/qa/specs/RequestSpecFactory.java
+++ src/test/java/com/alona/qa/specs/RequestSpecFactory.java
@@ -6,6 +6,8 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.config.HttpClientConfig;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.config.ObjectMapperConfig;
+import io.restassured.mapper.ObjectMapperType;
 
 import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
@@ -23,6 +25,8 @@ public final class RequestSpecFactory {
                 .accept("application/vnd.github+json")
                 .header("X-GitHub-Api-Version", ConfigManager.get("api.version"))
                 .config(config()
+                        .objectMapperConfig(ObjectMapperConfig.objectMapperConfig()
+                                .defaultObjectMapperType(ObjectMapperType.GSON))
                         .httpClient(HttpClientConfig.httpClientConfig()
                                 .setParam("http.connection.timeout", timeoutInSeconds * 1000)
                                 .setParam("http.socket.timeout", timeoutInSeconds * 1000)))
